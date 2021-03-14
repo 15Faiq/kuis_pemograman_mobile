9937d19e75909ce89b30c0c496996f8f488e110d
15faiq <maulanafaiq85@gmail.com>
2021-03-14 11:18:00 +0700

    kuis

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,138 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() {
-  runApp(MyApp());
+void main() => runApp(kertas());
+
+class kertas extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => Mainkertas();
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class Mainkertas extends State<kertas> {
+  final panjangAController = TextEditingController();
+  final panjangBController = TextEditingController();
+  final lebarAController = TextEditingController();
+  final lebarBController = TextEditingController();
+  double _inputpanjangA = 0;
+  double _inputlebarA = 0;
+  double _inputpanjangB = 0;
+  double _inputlebarB = 0;
+  double _panjang = 0;
+  double _lebar = 0;
+  double _luas = 0;
+
+  void rumuskertas() => setState(() {
+        _inputpanjangA = double.parse(panjangAController.text);
+        _inputlebarA = double.parse(lebarAController.text);
+        _inputpanjangB = double.parse(panjangBController.text);
+        _inputlebarB = double.parse(lebarBController.text);
+
+        _panjang = _inputpanjangA / _inputpanjangB;
+        _lebar = _inputlebarA / _inputlebarB;
+        _luas = _panjang * _lebar;
+      });
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Aplikasi Potong Kertas"),
+        ),
+        body: Container(
+          margin: EdgeInsets.all(8),
+          child: Column(
+            children: [
+              Text(
+                "Luas Kertas Plano",
+                style: TextStyle(fontSize: 18),
+              ),
+              TextFormField(
+                controller: panjangAController,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Panjang Kertas Plano",
+                    hintText: "Masukan Panjang kertas",
+                    border: OutlineInputBorder()),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                controller: lebarAController,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Lebar Kertas Plano",
+                    hintText: "Masukan Lebar kertas",
+                    border: OutlineInputBorder()),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                "Luas Kertas ",
+                style: TextStyle(fontSize: 18),
+              ),
+              TextFormField(
+                controller: panjangBController,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Panjang Kertas",
+                    hintText: "Masukan Panjang kertas",
+                    border: OutlineInputBorder()),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                controller: lebarBController,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Lebar Kertas",
+                    hintText: "Masukan Lebar kertas",
+                    border: OutlineInputBorder()),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                width: 150,
+                // ignore: deprecated_member_use
+                child: RaisedButton(
+                  child: Text(
+                    "Potong",
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                  onPressed: rumuskertas,
+                  color: Colors.red,
+                ),
+              ),
+              
+              Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('Hasil',
+                        style: TextStyle(color: Colors.black, fontSize: 20)),
+                    Text(
+                      _luas.toStringAsFixed(0),
+                      style: TextStyle(fontSize: 20),
+                    ),
+                     
+                  ],
+                ),
+                margin: EdgeInsets.all(8),
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
